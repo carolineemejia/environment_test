3dd97e9b5513be3faa8fa5ff03a642b37648ab0e
carolineemejia <hi123484@tamu.edu>
2022-09-15 17:29:26 -0500

    Editing test files

---
+++ spec/unit/unit_spec.rb
@@ -0,0 +1,28 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'JK', price: '1.11', published_date: '2020-01-23')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+  
+end
