b6885ce284e4faa9be349042400cd94ecad20905
carolineemejia <hi123484@tamu.edu>
2022-09-16 11:23:15 -0500

    Test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Book.create!([
-    [{title: 'Aggies'}, {title: 'WorkPlz'}]
+    {title: 'Aggies', author: 'Rev', price: '20.23', published_date: '2020-01-23'}, 
 ])
